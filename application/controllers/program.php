7caab11508195c08ea79550ccdbae66b2549950d
resaberlian <rezaberlian4@gmail.com>
2019-01-22 17:46:07 +0700

    initial commit

---
+++ application/controllers/program.php
@@ -0,0 +1,19 @@
+<?php
+
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class program extends CI_Controller {
+
+    public function index()
+    {
+        $this->load->view('viewprogram');
+        
+    }
+
+}
+
+/* End of file program.php */
+
+
+?>
